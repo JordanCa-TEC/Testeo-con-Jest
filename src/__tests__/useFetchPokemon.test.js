c1433df9d957d90d765bcfc06598756275c7b09c
email <jordanmorel.ca@gmail.com>
2024-10-16 18:10:57 -0500

    modificaciones 3

--- src/__tests__/useFetchPokemon.test.js
+++ src/__tests__/useFetchPokemon.test.js
@@ -1,3 +1,4 @@
+/*
 import axios from 'axios';
 import { renderHook, act } from '@testing-library/react-hooks';
 import useFetchPokemon from '../hooks/useFetchPokemon'; // Reemplaza con tu hook o función
@@ -37,4 +38,39 @@ describe('useFetchPokemon', () => {
     expect(result.current.data).toBe(null);
   });
 });
+*/
+import axios from 'axios';
+import { renderHook, act } from '@testing-library/react-hooks';
+import useFetchPokemon from '../hooks/useFetchPokemon'; // Ajusta según la ruta de tu hook
 
+jest.mock('axios');
+
+describe('useFetchPokemon', () => {
+  it('fetches and returns data successfully', async () => {
+    const mockData = { name: 'pikachu' };
+    axios.get.mockResolvedValueOnce({ data: mockData });
+
+    const { result, waitForNextUpdate } = renderHook(() => 
+      useFetchPokemon('https://pokeapi.co/api/v2/pokemon/pikachu')
+    );
+
+    await waitForNextUpdate(); // Esperar a que el estado se actualice
+
+    expect(result.current.data).toEqual(mockData);
+    expect(result.current.error).toBe(null);
+  });
+
+  it('handles fetch failure', async () => {
+    const errorMessage = 'Network Error';
+    axios.get.mockRejectedValueOnce(new Error(errorMessage));
+
+    const { result, waitForNextUpdate } = renderHook(() => 
+      useFetchPokemon('https://pokeapi.co/api/v2/pokemon/pikachu')
+    );
+
+    await waitForNextUpdate(); // Esperar a que el estado se actualice
+
+    expect(result.current.data).toBe(null);
+    expect(result.current.error.message).toEqual(errorMessage);
+  });
+});
