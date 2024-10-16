e4e7a657f28a4f2cc44d6a8c743428ee6d0501f8
email <jordanmorel.ca@gmail.com>
2024-10-16 17:38:50 -0500

    mod 1

--- src/__tests__/useFetchPokemon.test.js
+++ src/__tests__/useFetchPokemon.test.js
@@ -1,42 +1,40 @@
-import { renderHook, act } from '@testing-library/react-hooks';
 import axios from 'axios';
-import useFetchPokemon from '../hooks/useFetchPokemon';
+import { renderHook, act } from '@testing-library/react-hooks';
+import useFetchPokemon from '../hooks/useFetchPokemon'; // Reemplaza con tu hook o función
 
 jest.mock('axios');
 
 describe('useFetchPokemon', () => {
-  const mockData = { name: 'Pikachu', height: 1, weight: 6 };
-
-  it('should return data when the fetch is successful', async () => {
+  it('fetches and returns data successfully', async () => {
+    const mockData = { name: 'pikachu' }; 
     axios.get.mockResolvedValue({ data: mockData });
 
-    const { result, waitForNextUpdate } = renderHook(() =>
+    const { result, waitForNextUpdate } = renderHook(() => 
       useFetchPokemon('https://pokeapi.co/api/v2/pokemon/pikachu')
     );
 
-    expect(result.current.loading).toBe(true);
+    await act(async () => {
+      await waitForNextUpdate();
+    });
 
-    await waitForNextUpdate();
-
-    expect(result.current.loading).toBe(false);
     expect(result.current.data).toEqual(mockData);
     expect(result.current.error).toBe(null);
   });
 
-  it('should set an error if the fetch fails', async () => {
+  it('handles fetch failure', async () => {
     const errorMessage = 'Network Error';
     axios.get.mockRejectedValue(new Error(errorMessage));
 
-    const { result, waitForNextUpdate } = renderHook(() =>
+    const { result, waitForNextUpdate } = renderHook(() => 
       useFetchPokemon('https://pokeapi.co/api/v2/pokemon/pikachu')
     );
 
-    expect(result.current.loading).toBe(true);
+    await act(async () => {
+      await waitForNextUpdate();
+    });
 
-    await waitForNextUpdate();
-
-    expect(result.current.loading).toBe(false);
+    expect(result.current.error.message).toEqual(errorMessage); // Asegúrate de comparar con .message
     expect(result.current.data).toBe(null);
-    expect(result.current.error.message).toBe(errorMessage);
   });
 });
+
