541add70cc57df9b34d5ed3ac45bfa0c600520d0
email <jordanmorel.ca@gmail.com>
2024-10-16 19:58:25 -0500

    arreglo pasivo de app.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,7 +3,6 @@ import App from '../App';
 
 test('renders welcome message', () => {
   render(<App />);
-  // Modificado: Cambié el texto que se busca
   const welcomeElement = screen.getByText(/Welcome to the Pokémon App/i);
   expect(welcomeElement).toBeInTheDocument();
 });
