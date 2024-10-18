95a7827d93f23a84b39d78cf954d68757f25db27
email <jordanmorel.ca@gmail.com>
2024-10-17 21:27:47 -0500

    Codigo revision 02

--- src/__tests__/useFetchPokemon.test.js
+++ src/__tests__/useFetchPokemon.test.js
@@ -1,40 +1,3 @@
-/*
-import axios from 'axios';
-import { renderHook, act } from '@testing-library/react-hooks';
-import useFetchPokemon from '../hooks/useFetchPokemon'; // Ajusta según la ruta de tu hook
-
-jest.mock('axios');
-
-describe('useFetchPokemon', () => {
-  it('fetches and returns data successfully', async () => {
-    const mockData = { name: 'pikachu' };
-    axios.get.mockResolvedValueOnce({ data: mockData });
-
-    const { result, waitForNextUpdate } = renderHook(() => 
-      useFetchPokemon('https://pokeapi.co/api/v2/pokemon/pikachu')
-    );
-
-    await waitForNextUpdate(); // Esperar a que el estado se actualice
-
-    expect(result.current.data).toEqual(mockData);
-    expect(result.current.error).toBe(null);
-  });
-
-  it('handles fetch failure', async () => {
-    const errorMessage = 'Network Error';
-    axios.get.mockRejectedValueOnce(new Error(errorMessage));
-
-    const { result, waitForNextUpdate } = renderHook(() => 
-      useFetchPokemon('https://pokeapi.co/api/v2/pokemon/pikachu')
-    );
-
-    await waitForNextUpdate(); // Esperar a que el estado se actualice
-
-    expect(result.current.data).toBe(null);
-    expect(result.current.error.message).toEqual(errorMessage);
-  });
-});
-*/
 import axios from 'axios';
 import { renderHook, act } from '@testing-library/react-hooks';
 import useFetchPokemon from '../hooks/useFetchPokemon';
@@ -51,9 +14,8 @@ describe('useFetchPokemon', () => {
       useFetchPokemon('https://pokeapi.co/api/v2/pokemon/pikachu')
     );
 
-    // Usar act para esperar la actualización
     await act(async () => {
-      await waitForNextUpdate(); // Esto debe estar dentro de act
+      await waitForNextUpdate(); 
     });
 
     expect(result.current.data).toEqual(mockData);
@@ -69,7 +31,7 @@ describe('useFetchPokemon', () => {
     );
 
     await act(async () => {
-      await waitForNextUpdate(); // También aquí
+      await waitForNextUpdate();
     });
 
     expect(result.current.data).toBe(null);
