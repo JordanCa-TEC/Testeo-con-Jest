7e226e09f3a9883f05e39eac1d9b91fe382898ad
email <jordanmorel.ca@gmail.com>
2024-10-17 21:34:08 -0500

    ajustando para la prueba de coverage

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest', 
   },
   transformIgnorePatterns: [
     "/node_modules/(?!axios).+\\.js$",
@@ -13,4 +13,12 @@ module.exports = {
     'node_modules',
     'src',
   ],
+  collectCoverage: true, 
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/index.js", 
+    "!src/serviceWorker.js", 
+  ],
+  coverageReporters: ["text", "lcov"], 
 };
+
